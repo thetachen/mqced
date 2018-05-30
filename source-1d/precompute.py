d397b9f6e6a15ea8e0714708bcab1c75c5bae7ee
Theta@Nikogi <thetachen@gmail.com>
2018-05-30 19:09:55 -0400

    add precompute overlap

--- source-1d/precompute.py
+++ source-1d/precompute.py
@@ -13,6 +13,13 @@ from MaxwellPropagator import *
 from SystemPropagator import *
 
 
+#Default Options:
+ShowAnimation = True
+AveragePeriod = 100
+UseInitialRandomPhase = True
+NumberTrajectories = 1
+UsePlusEmission = True
+UseRandomEB = False
 
 
 if (len(argv) == 1):
@@ -25,13 +32,174 @@ elif (len(argv) == 3):
     execfile(argv[1])
     outfile = argv[2]
 
-#Default Options:
-ShowAnimation = True
-AveragePeriod = 100
-UseInitialRandomPhase = True
-NumberTrajectories = 1
-UsePlusEmission = True
-UseRandomEB = False
+def precompute_showoverlap(param_EM,param_TLS,rescaling_time,ShowAnimation=False):
+    """
+    Initialize
+    """
+    # dipole moment (Xi): Px, Py
+    Px = np.zeros(param_EM.NZgrid)
+    Py = np.zeros(param_EM.NZgrid)
+
+    # curl P (for rescale B and keep div B =0)
+    dPxdz = np.zeros(param_EM.NZgrid)
+    dPydz = np.zeros(param_EM.NZgrid)
+
+    # curl (curl P) (for rescale E)
+    d2Pxdz = np.zeros(param_EM.NZgrid)
+    d2Pydz = np.zeros(param_EM.NZgrid)
+
+    d3Pxdz = np.zeros(param_EM.NZgrid)
+    d3Pydz = np.zeros(param_EM.NZgrid)
+
+    # TE and TB
+    TEx = np.zeros(param_EM.NZgrid)
+    TEy = np.zeros(param_EM.NZgrid)
+    TBx = np.zeros(param_EM.NZgrid)
+    TBy = np.zeros(param_EM.NZgrid)
+
+    # a long vector [Ex,Ey,Bx,By]
+    EB = np.zeros(4*param_EM.NZgrid)
+
+    # energy change
+    Overlap = np.zeros((2,len(times)))
+
+    # initialize Px,Py,dPxdz, dPydz, d2Pxdz, d2Pydz
+    for iz in range(param_EM.NZgrid):
+        Z = param_EM.Zgrid[iz]
+        Px[iz] = param_TLS.Pmax * np.sqrt(param_TLS.Sigma/np.pi) * np.exp( -param_TLS.Sigma * (Z-param_TLS.Mu)**2 )
+        Py[iz] = 0.0
+        dPxdz[iz] = param_TLS.Pmax * np.sqrt(param_TLS.Sigma/np.pi) * np.exp( -param_TLS.Sigma * (Z-param_TLS.Mu)**2 ) * -param_TLS.Sigma * (Z-param_TLS.Mu)*2
+        dPydz[iz] = 0.0
+        d2Pxdz[iz] = param_TLS.Pmax * np.sqrt(param_TLS.Sigma/np.pi) * np.exp( -param_TLS.Sigma * (Z-param_TLS.Mu)**2 ) * \
+					  (-param_TLS.Sigma *2) \
+					+ param_TLS.Pmax * np.sqrt(param_TLS.Sigma/np.pi) * np.exp( -param_TLS.Sigma * (Z-param_TLS.Mu)**2 ) * \
+					  (-param_TLS.Sigma * (Z-param_TLS.Mu)*2) * \
+					  (-param_TLS.Sigma * (Z-param_TLS.Mu)*2)
+        d2Pydz[iz] = 0.0
+        # d3Pxdz[iz] = param_TLS.Pmax * np.sqrt(param_TLS.Sigma/np.pi) * np.exp( -param_TLS.Sigma * (Z-param_TLS.Mu)**2 ) * \
+		# 			  (-param_TLS.Sigma *2) * \
+        #               (-param_TLS.Sigma * (Z-param_TLS.Mu)*2)\
+		# 			+ param_TLS.Pmax * np.sqrt(param_TLS.Sigma/np.pi) * np.exp( -param_TLS.Sigma * (Z-param_TLS.Mu)**2 ) * \
+        #               (-param_TLS.Sigma * (Z-param_TLS.Mu)*2) * \
+		# 			  (-param_TLS.Sigma * (Z-param_TLS.Mu)*2) * \
+		# 			  (-param_TLS.Sigma * (Z-param_TLS.Mu)*2)\
+		# 			+ param_TLS.Pmax * np.sqrt(param_TLS.Sigma/np.pi) * np.exp( -param_TLS.Sigma * (Z-param_TLS.Mu)**2 ) * \
+		# 			  (-param_TLS.Sigma *2) * \
+		# 			  (-param_TLS.Sigma * (Z-param_TLS.Mu)*2) *2
+        d3Pxdz[iz] = param_TLS.Pmax * np.sqrt(param_TLS.Sigma/np.pi) * np.exp( -param_TLS.Sigma * (Z-param_TLS.Mu)**2 ) * \
+					  (12*(param_TLS.Sigma**2)*Z -8*(param_TLS.Sigma**3)*Z**3)
+        d3Pydz[iz] = 0.0
+
+    """
+    Direction of rescaling in E and B
+    we add additional term to minimize the interference
+    1. E filed: let TE(0)=0
+        TE = -curl^2(P) + 2a*P
+    2. B field: let dTBdz(0) = 0 (flat near the center)
+        TB = -curl(P) + (1/6a)*curl^3(P)
+    """
+    TEx = d2Pxdz + (2*param_TLS.Sigma)*Px
+    # TEx = d2Pxdz + d4Pxdz *(1.0/6/param_TLS.Sigma)
+    TBy = -dPxdz  - (1.0/6/param_TLS.Sigma)*d3Pxdz
+
+
+    # create EM object
+    EMP = EhrenfestPlusREB_MaxwellPropagator_1D(param_EM)
+    EMP.initializeODEsolver(EB,T0)
+    EMP.update_TETB(TEx,TEy,TBx,TBy)
+    EMP.applyAbsorptionBoundaryCondition()
+
+    #normalized
+    TEx = TEx/np.sqrt(EMP.TE2)
+    TBy = TBy/np.sqrt(EMP.TB2)
+    EMP.update_TETB(TEx,TEy,TBx,TBy)
+
+    """
+    Start Time Evolution
+    """
+    for it in range(len(times)+1):
+
+        print it
+        dE = 1E-2
+        sign = 1.0
+        if it == rescaling_time:
+            alpha,beta=EMP.MakeTransition_sign(dE,sign,UseRandomEB=False)
+        #print alpha,beta
+        #3. Evolve the field
+        EMP.propagate(dt)
+
+        #5. Apply absorption boundary condition
+        EMP.applyAbsorptionBoundaryCondition()
+        #EB = EMP.EB
+
+
+        """
+        Plot
+        """
+        if it%AveragePeriod==0 and ShowAnimation:
+            plt.sca(ax[0])
+            plt.cla()
+            ax[0].fill_between(EMP.Zgrid,0.0,np.sqrt(AU.E0)*(EMP.EB[EMP._Ex:EMP._Ex+EMP.NZgrid]),alpha=0.5,color='red',label='$E_x$')
+            ax[0].fill_between(EMP.Zgrid,0.0,np.sqrt(AU.E0)*(EMP.EB[EMP._Ey:EMP._Ey+EMP.NZgrid]),alpha=0.5,color='orange',label='$E_y$')
+            ax[0].plot(EMP.Zgrid,EMP.TEx,'--',color='red',lw=2)
+            ax[0].plot(EMP.Zgrid,EMP.TBy,'--',color='green',lw=2)
+            ax[0].axvline(x=param_TLS.Mu, color='k', linestyle='--')
+            ax[0].legend()
+
+            plt.sca(ax[1])
+            plt.cla()
+            #ax[1].fill_between(EMP.Zgrid,0.0,np.sqrt(AU.E0)*(EMP.EB[EMP._Ex:EMP._Ex+EMP.NZgrid]),alpha=0.5,color='red',label='$E_x$')
+            #ax[1].fill_between(EMP.Zgrid,0.0,np.sqrt(AU.E0)*(EMP.EB[EMP._Ey:EMP._Ey+EMP.NZgrid]),alpha=0.5,color='orange',label='$E_y$')
+            ax[1].fill_between(EMP.Zgrid,0.0,(np.array(EMP.EB[EMP._Ex:EMP._Ex+EMP.NZgrid])**2+np.array(EMP.EB[EMP._By:EMP._By+EMP.NZgrid])**2),alpha=0.5,color='black',label='$E_x^2+B_y^2$')
+            # ax[1].set_ylim([0,0.0001])
+            ax[1].axvline(x=param_TLS.Mu, color='k', linestyle='--')
+            ax[1].legend()
+
+            plt.sca(ax[2])
+            plt.cla()
+            ax[2].fill_between(EMP.Zgrid,0.0,EMP.EB[EMP._Bx:EMP._Bx+EMP.NZgrid],alpha=0.5,color='blue',label='$B_x$')
+            ax[2].fill_between(EMP.Zgrid,0.0,EMP.EB[EMP._By:EMP._By+EMP.NZgrid],alpha=0.5,color='green',label='$B_y$')
+            ax[2].legend(loc='best')
+
+
+            plt.sca(ax[3])
+            plt.cla()
+            #ax[3].plot(times[:it]*AU.fs,Ut[0,:it]-Ut[0,0],lw=2,label='ele energy')
+            #ax[3].plot(times[:it]*AU.fs,-(Ut[1,:it]-Ut[1,0]),lw=2,label='EM energy')
+            #ax[3].plot(times[:it]*AU.fs,dEnergy[0,:it], lw=2, label='coherent')
+            #ax[3].plot(times[:it]*AU.fs,dEnergy[1,:it], lw=2, label='incoherent')
+            #ax[3].plot(times[:it]*AU.fs,(-np.log(dEnergy[1,:it])+np.log(dEnergy[1,0]))/times[:it], lw=2, label='incoherent')
+            #ax[3].plot(times[:it]*AU.fs,Ut[0,:it]-Ut[0,0]+Ut[1,:it]-Ut[1,0],lw=2,label='energy diff')
+            ax[3].plot(EMP.Xs,np.array(EMP.Es)**2,lw=1,label='$E^2$')
+            ax[3].plot(EMP.Xs,np.array(EMP.Bs)**2,lw=1,label='$B^2$')
+            ax[3].legend(loc='best')
+            # ax[3].set_xlim([0,Tmax*AU.fs])
+            # ax[3].set_ylim([-max(EMP.Es),max(EMP.Es)])
+            ax[3].set_xlabel('x')
+            ax[3].legend()
+
+            # plt.sca(ax[4])
+            # plt.cla()
+            # fft_Ex = np.fft.rfft(EMP.Es[::-1])
+            # fft_Freq = np.array(range(len(fft_Ex))) * 2*np.pi /(EMP.Xs[0]-EMP.Xs[-1])
+            # ax[4].plot(fft_Freq,np.abs(fft_Ex)**2,lw=1,color='b',label='fft($E$)')
+            # ax[4].set_xlabel('$\omega$')
+            # ax[4].legend()
+            # fig.savefig('test_plot.pdf')
+            fig.canvas.draw()
+
+    """
+    End of Time Evolution
+    """
+
+    output={
+        'Zgrid':EMP.Zgrid,
+        'Ex':   EMP.EB[EMP._Ex:EMP._Ex+EMP.NZgrid],
+        'By':   EMP.EB[EMP._By:EMP._By+EMP.NZgrid],
+        'TEx':  EMP.TEx,
+        'TBy':  EMP.TBy,
+    }
+    return output
 
 def precompute(param_EM,param_TLS,ShowAnimation=False):
     """
@@ -246,6 +414,13 @@ if ShowAnimation:
 	plt.rc('font', family='Times New Roman', size='12')
 	plt.ion()
 	fig, ax= plt.subplots(5,figsize=(10.0,12.0))
+
+data = []
+for rescaling_time in rescaling_times:
+    output = precompute_showoverlap(param_EM,param_TLS,rescaling_time,ShowAnimation=False)
+    data.append(output)
+with open(outfile, 'wb') as f:
+    pickle.dump(data,f)
 
 data = []
 for i in range(NumberTrajectories):
---
+++ source-1d/test_precompute/plot_paper.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+import sys
+import gc
+sys.path.append("/home/theta/sync/mqced/source-1d")
+import numpy as np
+import cPickle as pickle
+from glob import glob
+from matplotlib import pyplot as plt
+from matplotlib.ticker import MultipleLocator, AutoLocator, MaxNLocator
+import matplotlib.gridspec as gridspec
+from utility import *
+plt.rc('text', usetex=True)
+plt.rc('font', family='Times New Roman', size='9')
+xsize = 8.6 #cm
+ysize = 20 #cm
+cm2inch = 1.0/2.54
+t2fs = 0.01
+x2nm = 2.998
+e2ev = 65.82
+
+W0 = 0.25
+P0 = 0.025*np.sqrt(2.0)
+field_unit = W0*P0
+
+syncoutput = "/home/theta/sync/mqced/documents/"
+prefix = "source-1d.test_2ls_ehrenfest+."
+def main():
+    fig, ax= plt.subplots(2,1,figsize=(xsize*cm2inch,8.0*cm2inch))
+    plot_Field(ax,'data.pkl',show_legend=False)
+    fig.tight_layout(pad=0.3, w_pad=0.0, h_pad=0.0)
+    # fig.text(0.0,0.97,'(a)', fontsize=9)
+    fig.savefig('fig.pdf')
+    fig.savefig(syncoutput+prefix+'fig.pdf')
+    plt.show(block=False)
+
+    plt.show()
+
+
+def plot_Field(ax,datafile,show_legend=True):
+
+    with open(datafile, 'r') as file:
+        data = pickle.load(file)
+        for d in data:
+            D = Struct(**d)
+
+            ax[0].fill_between(np.array(D.Zgrid)*x2nm,0.0,D.Ex/field_unit,color='red', lw=1,alpha=0.2, label=r'Eh+R')
+            ax[1].fill_between(np.array(D.Zgrid)*x2nm,0.0,D.By/field_unit,color='green', lw=1,alpha=0.2, label=r'Eh+R')
+        ax[0].plot(np.array(D.Zgrid)*x2nm,D.TEx*0.1/field_unit,'--',color='red', lw=2,alpha=0.8, label=r'Eh+R')
+        ax[1].plot(np.array(D.Zgrid)*x2nm,D.TBy*0.1/field_unit,'--',color='green', lw=2,alpha=0.8, label=r'Eh+R')
+    # ax.set_ylim([-5E-3,5E-3])
+    ax[0].set_xlim([-20.0,20.0])
+    ax[0].set_xlabel(r'$x$ [nm]')
+    ax[0].set_ylabel(r'$E_z$')
+    ax[1].set_xlim([-20.0,20.0])
+    ax[1].set_xlabel(r'$x$ [nm]')
+    ax[1].set_ylabel(r'$B_y$')
+    # ax.text(2.5E4,4.0E-5,r'$\rho_{11}='+str(C0)+'$')
+    if show_legend: ax.legend(fontsize=8,loc='upper left')
+
+    # ax.ticklabel_format(style='sci',scilimits=(0,0),axis='both',useLocale=True)
+
+
+if __name__ == '__main__':
+    main()
